a7ed97992e29d384040c85207d4454bc003c2977
Janet Young <jayoung@fredhutch.org>
2022-06-28 11:07:55 -0700

    add example shell script for bwa

---
+++ example_scripts/bwa_mem_paired.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /app/lmod/lmod/init/profile
+module load SAMtools/1.11-GCC-10.2.0
+module load BWA/0.7.17-GCC-10.2.0
+
+(bwa mem -t 4 REFGENOME.fa mySample_R1.fq.gz mySample_R2.fq.gz | samtools view -@ 4 -Sb -  | samtools sort -@ 4 -O bam -T ${DELETE30}/malik_h/mySample_tmpDir > ./mySample.bwa.bam ) 2>> ./mySample.bwa.log.txt
+
+samtools index ./new_parental_paired_trim.bwa.bam
+
+samtools flagstat ./new_parental_paired_trim.bwa.bam > ./new_parental_paired_trim.bwa.bam.flagstats
+
+module purge
